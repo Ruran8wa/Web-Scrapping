3537cc0abb4a44290a09d5bf61690008482d6eb6
ruran8wa <p.rurangwa@alustudent.com>
2024-02-16 21:36:01 +0200

    scrap

---
+++ README.md
@@ -0,0 +1,59 @@
+# Smartphone Pricing Data Scraping and Visualization
+
+## Overview
+
+This project focuses on scraping smartphone pricing data from various online retailers using Selenium WebDriver and then visualizing the pricing trends using Chart.js and jQuery.
+
+## System Description
+
+The system consists of a Node.js application that utilizes Selenium WebDriver for web scraping. It also employs Express, EJS, Chart.js, and jQuery for creating interactive visualizations. The primary objective is to offer insights into smartphone pricing trends over time across different retailers.
+
+## Prerequisites
+
+- Node.js installed
+- npm packages (express, ejs, selenium-webdriver)
+
+## Setup
+
+1. **Clone the repository**
+
+   ```bash 
+   git clone https://github.com/Ruran8wa/Web-Scrapping.git
+   cd web_scraping
+   ```
+
+2. **Install dependencies**
+
+	```bash
+	npm install
+	```
+
+3. **Run the scraping script and start the server**
+
+	```bash
+	node script.js
+	```
+
+4. **Open in Browser**
+
+	Open your web browser and navigate to the following link to view the visualization.
+
+	```bash
+	http://localhost:5500/
+	```
+
+## Project Structure
+
+- **script.js**: Contains the web scraping script using Selenium and the Express server setup.
+- **public/scraped_data.json**: JSON file containing the scraped smartphone pricing data.
+- **views/index.ejs**: HTML template for rendering the visualization using Chart.js and jQuery.
+
+## Visualization
+
+The visualization displays a line chart showcasing the pricing trends of smartphones over time. The chart includes interactive features such as zooming, panning, and tooltips displaying detailed information.
+
+## Acknowledgments
+
+- The project utilizes Chart.js for creating interactive charts.
+- Selenium WebDriver is employed for web scraping smartphone pricing data.
+- Express and EJS are used to set up the server and render the HTML template.
---
+++ script.js
@@ -0,0 +1,187 @@
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+
+const Chart = require("chart.js");
+
+const { Builder, By } = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+
+const options = new firefox.Options();
+options.addArguments("--headless");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+const scrapedData = [];
+
+// Function to scrape smartphone pricing data from kigali discount website
+async function scrapeWebsite1() {
+  let driver = await new Builder()
+    .forBrowser("firefox")
+    .setFirefoxOptions(options)
+    .build();
+
+  try {
+    const url =
+      "https://www.kigalidiscount.com/collections/cell-phones-smartphones";
+    await driver.get(url);
+    const retailer = "kigalidiscount";
+
+    const smartphoneNames = await driver.findElements(
+      By.css(".product-item__title")
+    );
+    const smartphonePrices = await driver.findElements(By.css(".price"));
+
+    const dateTimeScraped = new Date().toLocaleString();
+
+    for (let i = 0; i < smartphoneNames.length; i++) {
+      const smartphoneName = await smartphoneNames[i].getText();
+      const smartphonePrice = await smartphonePrices[i].getText();
+
+      addScrapedData(
+        smartphoneName,
+        smartphonePrice,
+        retailer,
+        dateTimeScraped
+      );
+
+      console.log("Website:", url);
+      console.log("Smartphone Name:", smartphoneName);
+      console.log("Smartphone Price:", smartphonePrice);
+      console.log("Date/Time Scraped:", dateTimeScraped);
+      console.log("---------------------------------------");
+    }
+  } catch (error) {
+    console.error("An error occurred while scraping from Website 1:", error);
+  } finally {
+    await driver.quit();
+  }
+}
+
+// Function to scrape smartphone pricing data from newspecies website
+async function scrapeWebsite2() {
+  let driver = await new Builder()
+    .forBrowser("firefox")
+    .setFirefoxOptions(options)
+    .build();
+
+  try {
+    const url = "https://thenewspecies.com/product-category/smartphone/";
+    await driver.get(url);
+    const retailer = "thenewspecies";
+
+    const smartphoneNames = await driver.findElements(
+      By.css(".woocommerce-loop-product__title")
+    );
+    const smartphonePrices = await driver.findElements(By.css(".price"));
+
+    const dateTimeScraped = new Date().toLocaleString();
+
+    for (let i = 0; i < smartphoneNames.length; i++) {
+      const smartphoneName = await smartphoneNames[i].getText();
+      const smartphonePrice = await smartphonePrices[i].getText();
+
+      addScrapedData(
+        smartphoneName,
+        smartphonePrice,
+        retailer,
+        dateTimeScraped
+      );
+    }
+  } catch (error) {
+    console.error("An error occurred while scraping from Website 1:", error);
+  } finally {
+    await driver.quit();
+  }
+}
+
+// Function to scrape smartphone pricing data from zimcompass website
+async function scrapeWebsite3() {
+  let driver = await new Builder()
+    .forBrowser("firefox")
+    .setFirefoxOptions(options)
+    .build();
+
+  try {
+    const retailer = "zimcompass";
+    const url = "https://rw.zimcompass.com/phones";
+    await driver.get(url);
+
+    const smartphoneNames = await driver.findElements(By.css(".description"));
+    const smartphonePrices = await driver.findElements(By.css(".price"));
+
+    const dateTimeScraped = new Date().toLocaleString();
+
+    for (let i = 0; i < smartphoneNames.length; i++) {
+      const smartphoneName = await smartphoneNames[i].getText();
+      const smartphonePrice = await smartphonePrices[i].getText();
+
+      addScrapedData(
+        smartphoneName,
+        smartphonePrice,
+        retailer,
+        dateTimeScraped
+      );
+
+      console.log("Website:", url);
+      console.log("Smartphone Name:", smartphoneName);
+      console.log("Smartphone Price:", smartphonePrice);
+      console.log("Date/Time Scraped:", dateTimeScraped);
+      console.log("---------------------------------------");
+    }
+  } catch (error) {
+    console.error("An error occurred while scraping from Website 1:", error);
+  } finally {
+    await driver.quit();
+  }
+}
+
+// Call each scraping function for each websites
+(async () => {
+  await scrapeWebsite1();
+  await scrapeWebsite2();
+  await scrapeWebsite3();
+
+  saveDataToFile();
+})();
+
+// Function to save scraped data to scrapped data file
+function saveDataToFile() {
+  const dataToSave = JSON.stringify(scrapedData);
+
+  fs.writeFile(
+    "public/scraped_data.json",
+    JSON.stringify(dataToSave, null, 2),
+    { encoding: "utf-8", flag: "w" },
+    (err) => {
+      if (err) throw err;
+      console.log("Scraped data saved to file.");
+    }
+  );
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const filePath = path.join(__dirname, "public", "scraped_data.json");
+    const data = await fs.readFile(filePath, "utf-8");
+    const jsonData = JSON.parse(data);
+
+    res.render("index", { jsonData });
+  } catch (error) {
+    console.error("Error reading file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Helper function to add scraped data to the array
+function addScrapedData(name, price, retailer, time) {
+  scrapedData.push({ name, price, retailer, time });
+}
+
+app.listen(5500, () => {
+  console.log("Server is running on port 5500");
+});
